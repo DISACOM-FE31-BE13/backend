5c28350f377e543c2724fc425a2faf64043947d2
GadingAulia <100004993+GadingAulia@users.noreply.github.com>
2023-06-13 13:03:40 +0700

    add index.js gading aulia

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use("/api/v1/join", joinRoute);
 app.use("/api/v1/motivasi", motivasiRoute);
 app.use("/", (req,res) =>{
   res.send("Welcome to the server home page")
-)}
+})
 
 app.listen(port, () => {
   connect();
